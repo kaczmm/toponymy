60ad21ae9bf4477f6bdf6cd1ecadf7b80c34e4b6
Matt Kaz <matthewkaczmarek1@gmail.com>
2026-01-10 22:08:50 -0500

    Almost done ck3 script

--- ck3_clean_titles.py
+++ ck3_clean_titles.py
@@ -4,26 +4,28 @@ def main():
     titles = []
     # read the whole file into a list
     with open("00_landed_titles.txt","r",encoding="utf-8-sig") as f:
-        for line in f:
-            if any(line.strip()[:2]==p for p in prefixes) or name_list in line:
-                titles.append(line.strip())
+        titles = f.readlines()
 
 	# line contains "cultural_names" we will traverse
     cname_start = -1
+    i = 0
     
     # write a new file that excludes 
     with open("CLEAN_landed_titles.txt","w",encoding="utf-8-sig") as f:
-        for i in range(len(titles)):
+        while i<len(titles):
             if "cultural_names" in titles[i] and "{" in titles[i]: # cultural names block!
                 print("Found cultural names block")
                 cname_start = i
                 for j in range(cname_start,len(titles)):
+                    print('...Line...',titles[j].strip())
                     if "}" in titles[j]:
                         i = j
-                        print("...found block close")
+                        print("......found block close")
                         break
             else:
-                f.write(titles[i]+"\n")
+                f.write(titles[i])
+            i+=1
+    print("Done!")
 
 
 if __name__ == '__main__':
--- ck3_script.py
+++ ck3_script.py
@@ -1,166 +1,184 @@
 # CK3 Toponymy Script
-#  1. load the csv and get a dictionary of titles:cn
-#  2. insert name list / cname blocks wherever needed (need to check if there's already a cname for the culture in titles)	
-#  3. make a localisation file with all the new localisation tags...
+#  1. load landed_titles.txt and make a list of its lines
+#  2. load the csv and get A) 2d list of cnames B) list of sets (index,culture)
+#  3. go through each culture (in list of sets) and create a list of dictionairies
+#  4. go through each line in landed_titles and check if it has a cname that can be updated
+#  5. export the result as a new landed_titles file
 
 def main():
-    # culture name (affects file and cn name tags)
-    culture = "default"
-    
-    # prompt for culture name
-    print("Enter culture name:")
-    try:
-        culture = input().lower()
-    except:
-        print("Error! Please enter a valid culture name")
-        input()
-        exit()
-        
-    # output mode: controls which messages get printed
-    outputMode = 0
-    print("Choose an output mode: \n[0] All log output \n[1] New localisation only \n[2] Localisation conflicts only")
-    try:
-        outputMode = int(input())
-    except:
-        print("Error! Invalid option")
-        input()
-        exit()
+    titles = []
+    toponymy = []
+    cultures = []
+    new_titles = []
+    dictionairies = []
+    qp = [] # quickpaste - temp store for lists so i dont have to write long lines
 
-    # scrape the csv
-    with open("toponymy.csv","r",encoding="utf-8-sig") as f:
-        lines = f.readlines()
-        T = len(lines)
-    csv = []
-    for line in lines:
-        csv.append(line.strip().split(','))
-    csv[0] = [l.lower() for l in csv[0]]
-    if outputMode==0: print("csv scraped successfully")
-
-    # find the column for our target culture
-    culture_pos = -1
-    culture_pos = csv[0].index(culture)
-    if culture_pos == -1:
-        print("Fatal error: no such culture in toponymy csv")
-        input()
-        exit()
-    if outputMode==0: print("Culture <",culture,"> found in csv at column number", culture_pos)
-
-    # build a dictionary from the csv
-    list_of_pairs = []
-    for i in range(1,T): # skip line 0 (column labels)
-        # csv[i][0] must be defined (ie this row is a ck3 title)
-        # csv[i][culture_pos] must be defined (ie the culture must have a name for this title)
-        # csv[i][culture_pos] must not be the same as csv[i][1] (ie the sanitised title; it must have a distinct cultural name)
-        if csv[i][0]!="" and csv[i][culture_pos]!="" and csv[i][culture_pos]!=csv[i][1]:
-            list_of_pairs.append( (csv[i][0],csv[i][culture_pos]) )
-    toponymy = dict(list_of_pairs)
-    if outputMode==0: print("dictionary built successfully")
-
-    # the array of lines of text from landed_titles.txt
+    #  1. load landed_titles.txt and make a list of its lines
     with open("00_landed_titles.txt","r",encoding="utf-8-sig") as f:
         titles = f.readlines()
-    if outputMode==0: print("00_landed_titles.txt read successfully")
 
-    # line pointer
-    i = 0
+    #  2. load the csv and get A) 2d list of cnames B) list of sets (index,culture)
+    with open("Toponymy Project - output.csv","r",encoding="utf-8-sig") as f:
+        # cultures = [(index,"name"),(index,"name"),..]
+        qp = f.readline().split(',')
+        reserved = ['ck3','sanitised','desc','iotised','?']
+        for i in range(len(qp)):
+            qp[i] = qp[i].replace("\n","").lower()
+            if not any(r in qp[i] for r in reserved):
+                if qp[i]!="": cultures.append((int(i),qp[i].lower().strip()))
+
+        for line in f.readlines():
+            toponymy.append(line.split(','))
+
+    #  3. go through each culture (in list of sets) and create a list of dictionairies
+    for c in cultures:
+        dictionairies.append(csv_scrape(titles,toponymy,c))
+
+    #  4. go through each line in landed_titles and check if it has a cname that can be updated
+    new_titles = titles
+    for d in dictionairies:
+        new_titles = culture_filter(new_titles,d)
 
     # remember where opening brackets are (-1 means not found)
     title_start = -1
     cn_start = -1
+    last_space = -1 # makes the block look nicer
 
     # stuff for keeping track of title name and ws
     title = ""
     ws = ""
+
+    #  5. export the result as a new landed_titles file
+    with open("NEW_landed_titles.txt","w",encoding="utf-8-sig") as f:
+        f.writelines(new_titles)
+
+    #  6. create localisation files
+    for d in dictionairies:
+        if len(list(d.items()))>3:
+            gen_localisation(d)
+
+    #  7. create a loc replace for the sanitised title names which should be fixed/changed
     
-    # name list name - used for checking definitions
-    name_list = "name_list_"+culture # ie name_list_polish
+    print("Done!\n")
 
-    if outputMode==0: print("Looking for titles...")
-    while (i < len(titles)):
-        # check if line is a title
-        i+=1
-        if (i == len(titles)):
-            if outputMode==0: print("Finished reading titles! Last read title was:",title)
-            break
-        line = titles[i].strip()
-        if line[0:2] in ['e_','k_','d_','c_','b_']:
-            # title name is from start of line until =
-            # strip method removes leading and trailing ws
-            title = line.split("=")[0].strip()
-            
-            # check if title has a cultural name
-            if title in toponymy:
-                if outputMode==0: print("Found definable title:",title)
-                # remember this line number
-                title_start = i
-                
-                # copy the indentation plus one tab
-                ws = lws(titles[i]) + "\t"
-                
-                # keep going until either "cultural_names" OR another title
-                # -1 indicates none found
-                cn_start = -1
-                if outputMode==0: print("Looking for cultural_names block...")
-                while True:
-                    i+=1
-                    line = titles[i].strip()
-                    
-                    # Found cultural_names block
-                    if line[:14] == "cultural_names":
-                        if outputMode==0: print("  Found cultural_names block at line",i)
-                        cn_start = i
-                    # Found next title -> no cn block
-                    elif line[0:2] in ['e_','k_','d_','c_','b_']:
-                        if outputMode==0: print("    Found next title",titles[i].split("=")[0].strip(),"at line",i)
-                        break
-                    # Found cn definition for this culture already
-                    elif name_list in line:
-                        if outputMode!=1: print("    Found existing definition",line,"at line",i)
-                        cn_start = -2 # special value to show definition exists (do nothing)
-                        break
-                    elif i >= len(titles):
-                        if outputMode==0: print("    Reached end of titles, nothing found.")
-                        break
-                i = title_start
+# takes a dictionary and creates a localisation file
+def gen_localisation(d):
+    culture = d["culture_name"]
 
-                # if it has a CN block, add to it
-                if cn_start > -1:
-                    titles.insert(cn_start+1, ws+"\tname_list_"+culture+" = "+"cn_"+title+"_"+culture+"\n")
-                    i = cn_start+1 #skip the new lines
-                    if outputMode!=2: print("Found cn block and added definition for",title,"-",toponymy[title])
-
-                # if it doesn't have a CN block, make one and add to it
-                # Add a line directly underneath cn_start
-                elif cn_start == -1:
-                    titles.insert(title_start+1, ws+"cultural_names = {\n")
-                    titles.insert(title_start+2, ws+"\tname_list_"+culture+" = "+"cn_"+title+"_"+culture+"\n")
-                    titles.insert(title_start+3, ws+"}\n")
-                    titles.insert(title_start+4, "\n")
-                    i = title_start+4 #skip the new lines and close bracket
-                    if outputMode!=2: print("Added new cn block and definition for",title,"-",toponymy[title])
-                
-                else:
-                    i = title_start
-                    if outputMode==0: print("Got lost, nothing found or added")
-                    
-                    
-    # write the new 00_landed_titles
-    with open("zz_00_landed_titles.txt","w",encoding="utf-8-sig") as f:
-        f.writelines(titles)
-    
     # create a localisation file
     loc = ['l_english:\n', ' # '+culture+' cultural names, from toponymy project\n']
-    for (t,c) in list_of_pairs:
-        loc.append(' cn_'+t+'_'+culture+':0 "'+c+'"\n')
-        loc.append(' cn_'+t+'_'+culture+'_adj:0 "'+c+'"\n')
+    for (t,c) in list(d.items()):
+        if t!="culture_pos" and t!="culture_name":
+            loc.append(' cn_'+t+'_'+culture+':0 "'+c+'"\n')
+            loc.append(' cn_'+t+'_'+culture+'_adj:0 "'+c+'"\n')
 
     # then just write this to a new file
-    loc_file_name = "zz_cn_"+culture+"_l_english.yml"
+    loc_file_name = "zztp_cn_"+culture+"_l_english.yml"
     with open(loc_file_name,"w",encoding="utf-8-sig") as f:
         f.writelines(loc)
 
-    print("Done!\n")
+# method that goes through a titles file and inserts cultural names for 1 culture
+def culture_filter(titles,d):
+    ts = titles
+    culture = d["culture_name"] # name of the culture being worked on
+    #culture_pos = int(d["culture_pos"]) # culture column position in toponymy
+    title_start = -1 # start of title block
+    nice_spot = -1
+    cn_start = -1 # start of cname block
+    t = "" # current title being looked at
+    ws = "" # placeholder for intentation whitespace
+    i = 0 # line pointer
 
+    name_list = "name_list_"+culture
+
+    print("Checking",culture)
+    while i < len(ts):
+        i += 1
+        if i >= len(ts):
+            print("...done", culture)
+            break
+        # check if title
+        elif is_title(ts[i]):
+            t = title_strip(ts[i])
+
+            # check if title has a cname defined for this culture
+            #  remember d = {("title","name"), ...}
+            #  dictionairies are (key,value) lists
+            if t in d.keys() and d[t]!=None and d[t]!="" and d[t]!="None":
+                #print("Found definable title:",t)
+                
+                # Bookmark this line so we can return later if needed
+                title_start = i
+                # Remember indentation for to add a line later
+                ws = lws(ts[i]) + "\t"
+
+                # continue until we find a cname block or another title
+                cn_start = -1
+                #print("..looking for cname block..")
+                for j in range(i,len(ts)):
+                    line = ts[j].strip()
+
+                    # cname block
+                    if is_cname_block(line):
+                        #print("....found cname block at line",j)
+                        cn_start = j
+                    # found a cname definition
+                    elif is_cname(line,culture):
+                        #print("....found existing cname:",line.strip())
+                        if not t in ts[j]: # ie NOT one of my generated ts (always of the form cn_b_title_culture)
+                            ts[j] = (lws(ts[j]) + name_list + " = " + "cn_"+t+"_"+culture + " # " + ts[j].strip().split("=")[1].strip() +"\n")
+                        i = j
+                        cn_start = -2 # special flag to show we already added applied the new cname
+                        break
+                    # cname block was found and we reached the next title
+                    elif cn_start > -1 and is_title(line):
+                        #print("....cname block was found and we reached the next title",j)
+                        i = j
+                        break
+                    elif "color = {" in line:
+                        nice_spot = j+1 # nice clean spot to insert cname block
+                        #print("found a nice spot for",t)
+
+                # if it has a cname block but no cname, add to the cname block
+                if cn_start > -1:
+                    ts.insert(cn_start+1, ws+"\t"+name_list+" = "+"cn_"+t+"_"+culture+"\n")
+                    #print("Added cname to existing cname block")
+                
+                # elif no cname block > make one and add to it
+                elif cn_start == -1:
+                    ts.insert(nice_spot, ws+r"cultural_names = {\n")
+                    ts.insert(nice_spot+1, ws+"\t"+name_list+" = "+"cn_"+t+"_"+culture+"\n")
+                    ts.insert(nice_spot+2, ws+"}\n")
+                    ts.insert(nice_spot+3, "\n")
+
+                # elif cn_start == -2: we don't need to do anything
+    #done
+    return ts
+
+def title_strip(line):
+    title = ""
+
+    # example line:
+    #  b_montereau = {
+    title = line.split(r"=")[0].strip()
+
+    return title
+
+# takes a line of text and tells you if contains a title declaration
+def is_title(line):
+    if any(line.strip()[:2]==p for p in ('b_','c_','d_','k_','e_')) and "{" in line:
+        return True
+    return False
+
+def is_cname_block(line):
+    if "cultural_names" in line and "=" in line and "{" in line:
+        return True
+    return False
+
+def is_cname(line,culture):
+    if "name_list_"+culture in line and "=" in line:
+        return True
+    return False
 
 # method for extracting exact lefthand whitespace
 def lws(input_string):
@@ -175,6 +193,37 @@ def lws(input_string):
 		else:
 			break
 	return ws
+
+# titles: list of lines in 00_landed_titles.txt
+# csv: list of rows in toponymy csv
+# pair: a pair containing (pos, name) of a culture in csv
+# returns a dictionary for the culture provided
+def csv_scrape(titles, csv, pair):
+    list_of_pairs = []
+    count = 0
+    culture_pos = int(pair[0])
+    culture = pair[1]
+
+	# go through CSV and grab anything for this culture
+    for row in csv:
+        row[culture_pos] = row[culture_pos].replace("\n","").strip()
+        if row[0]!= "" and row[culture_pos]!="" and row[culture_pos]!=None:
+            if "/" in row[culture_pos]: # avoid collisions
+                list_of_pairs.append((row[0], row[culture_pos].split(r"/")[0].strip()))
+            else:
+                list_of_pairs.append((row[0], row[culture_pos]))
+
+    if len(list_of_pairs) < 3:
+        print("..",len(list_of_pairs),"definitions for",culture)
+        for pair in list_of_pairs:
+            print("....",pair)
+
+    # position and name added to dictionary so we can look them up later
+    list_of_pairs.append(("culture_pos",culture_pos))
+    list_of_pairs.append(("culture_name",culture))
+
+
+    return dict(list_of_pairs)
 
 
 if __name__ == '__main__':
